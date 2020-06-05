a13926e9d34ad69133d62b69c6d71556e949c494
tamuraryoya <tamuraryoya@team-lab.com>
2020-06-05 22:44:32 +0900

    ✨ ルートページを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { Configuration } from '@nuxt/types';
 
 const config: Configuration = {
   srcDir: 'src',
+  buildModules: ['@nuxt/typescript-build'],
 };
 
 module.exports = config;
---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <h1>Hello, world.</h1>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class IndexPage extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>

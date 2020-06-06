68b36c1fedf5beeedc5168035297f58ba5a5e7ae
lollipop-onl <lollipop-onl@users.noreply.github.com>
2020-06-06 03:07:48 +0000

    👕 Fixed auto-fixable ESLint errors by github-actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
-import path from "path";
-import { Configuration } from "@nuxt/types";
+import path from 'path';
+import { Configuration } from '@nuxt/types';
 
 const config: Configuration = {
-  srcDir: "src",
-  buildModules: ["@nuxt/typescript-build"],
+  srcDir: 'src',
+  buildModules: ['@nuxt/typescript-build'],
   build: {
     loaders: {
       scss: {
         sassOptions: {
           // 参照のルートパスを追加
-          includePaths: [path.join(__dirname, "src/assets/styles")],
+          includePaths: [path.join(__dirname, 'src/assets/styles')],
         },
       },
     },

913243e0756cd4d884d06f3945b4f0c890a46239
tamuraryoya <tamuraryoya@team-lab.com>
2020-06-06 11:59:26 +0900

    🔧 Sassのリソースパスに対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
+import path from "path";
 import { Configuration } from "@nuxt/types";
 
 const config: Configuration = {
   srcDir: "src",
   buildModules: ["@nuxt/typescript-build"],
+  build: {
+    loaders: {
+      scss: {
+        sassOptions: {
+          // 参照のルートパスを追加
+          includePaths: [path.join(__dirname, "src/assets/styles")],
+        },
+      },
+    },
+  },
 };
 
 module.exports = config;
